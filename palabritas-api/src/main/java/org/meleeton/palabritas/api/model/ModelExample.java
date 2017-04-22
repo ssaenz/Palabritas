50a42ea21fee2aa4b73289dd6ef2f7460f7d62f4
Sergio Sáenz <ssaenz@indaba.es>
2017-04-22 15:56:47 +0200

    Addes model example.

---
+++ palabritas-api/src/main/java/org/meleeton/palabritas/api/model/ModelExample.java
@@ -0,0 +1,25 @@
+package org.meleeton.palabritas.api.model;
+
+public class ModelExample {
+	
+	private String exampleProperty;
+	
+	public ModelExample() { }
+
+	public ModelExample(String exampleProperty) {
+		super();
+		this.exampleProperty = exampleProperty;
+	}
+
+	public String getExampleProperty() {
+		return exampleProperty;
+	}
+
+	public void setExampleProperty(String exampleProperty) {
+		this.exampleProperty = exampleProperty;
+	}
+	
+	
+	
+
+}

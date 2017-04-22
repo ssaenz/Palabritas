495a26bb917b0c629edf8793dfc112b42c7b4a6b
Andoni <aserrano011@ikasle.ehu.es>
2017-04-22 19:43:04 +0200

    Model redefinition

--- palabritas-api/src/main/java/org/meleeton/palabritas/api/model/CommitWord.java
+++ palabritas-api/src/main/java/org/meleeton/palabritas/api/model/CommitWord.java
@@ -1,12 +1,9 @@
 package org.meleeton.palabritas.api.model;
 
-import java.util.List;
-
-public class Word {
+public class CommitWord {
 	
 	private String word;
 	private double occurrences;
-	private List<String> commits;
 	
 	public String getWord() {
 		return word;
@@ -21,13 +18,8 @@ public class Word {
 	public void setOccurrences(int occurrences) {
 		this.occurrences = occurrences;
 	}
-	
-	public void addCommit(String commitStr){
-		this.commits.add(commitStr);
-		incrementOccurrence();
-	}
-	private void incrementOccurrence(){
-		occurrences++;
+	public void sumOccurrence(){
+		this.occurrences++;
 	}
 	
 }

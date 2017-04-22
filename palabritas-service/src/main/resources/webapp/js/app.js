4d96bb3e752c945661a94108eb74787b5366414f
ojoven <mikeltorresmail@gmail.com>
2017-04-22 17:01:01 +0200

    Develop initial load + main AJAX retrieve function

--- palabritas-service/src/main/resources/webapp/js/app.js
+++ palabritas-service/src/main/resources/webapp/js/app.js
@@ -1,3 +1,41 @@
 $(function() {
-	console.log('Hello Palabritas');
+
+	initialLoad();
+
 });
+
+/** INITIAL LOAD OF DATA **/
+// Initial load of the page will render a list with the first most 10 used words
+function initialLoad() {
+
+	var offset = 0;
+	var limit = 10;
+	var view = 'list'; // Future views can include [list, tagcloud, history...]
+	getSortedWords(offset, limit, function(words) {
+		renderWords(words, view);
+	});
+
+}
+
+/** GET SORTED WORDS **/
+function getSortedWords(offset, limit, callback) {
+
+	var urlBase = 'http://localhost:8081/palabritas-service';
+	var url = urlBase + '/words';
+	var data = {};
+	data.offset = offset;
+	data.limit = limit;
+
+	$.get(url, data, function(data) {
+
+		callback(data.words);
+
+	});
+}
+
+// View dispatcher and word renderer
+function renderWords(words, view) {
+
+	// The render of the words will depend on the HTML structure
+
+}

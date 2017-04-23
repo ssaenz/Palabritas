dbb6ed9483cd3ac3d6163be5c8539289ffb97c0a
Sergio Sáenz <ssaenz@indaba.es>
2017-04-23 09:43:29 +0200

    Cleaned unnested classes. Implemented get words sorted

--- palabritas-persistence/src/main/java/org/meleeton/palabritas/persistence/resources/CommitRepository.java
+++
@@ -1,9 +0,0 @@
-package org.meleeton.palabritas.persistence.resources;
-
-import org.meleeton.palabritas.api.model.CommitWord;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CommitRepository extends MongoRepository<CommitWord, String>, CommitRepositoryCustom {
-	//TODO 
-	
-}

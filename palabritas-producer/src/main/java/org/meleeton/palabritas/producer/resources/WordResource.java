5a088b7505cf2f250aec042d318a0036f745614b
Sergio Sáenz <ssaenz@indaba.es>
2017-04-23 12:51:24 +0200

    Added offset and limit to the rest.

--- palabritas-producer/src/main/java/org/meleeton/palabritas/producer/resources/WordResource.java
+++ palabritas-producer/src/main/java/org/meleeton/palabritas/producer/resources/WordResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("words")
+@Path("commitwords")
 public interface WordResource {
 	
 	@GET

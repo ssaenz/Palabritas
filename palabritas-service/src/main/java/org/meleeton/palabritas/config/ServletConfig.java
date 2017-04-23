9e65c922387cb01d96636c5b3d2377b0c28fa5f8
Sergio Sáenz <ssaenz@indaba.es>
2017-04-23 16:39:56 +0200

    Added cors

--- palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
+++ palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
@@ -1,6 +1,7 @@
 package org.meleeton.palabritas.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -20,6 +21,13 @@ public class ServletConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index.html");
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/palabritas-service/**")
+	        .allowedOrigins("http://andoniserra.me:8081/")
+	        .allowedMethods("GET");
 	}
 }
 

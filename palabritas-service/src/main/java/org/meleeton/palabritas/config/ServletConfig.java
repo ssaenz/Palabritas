8d9f371a8e812653059338915c9f07accbf8e854
Sergio Sáenz <ssaenz@indaba.es>
2017-04-23 12:26:23 +0200

    Fixed bad configuration

--- palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
+++ palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
@@ -9,10 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class ServletConfig extends WebMvcConfigurerAdapter {
-
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+	        "classpath:/META-INF/resources/", "classpath:/resources/",
+	        "classpath:/static/", "classpath:/public/" };
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("/");
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
 	}
 
 	@Override

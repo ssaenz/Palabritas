05a971b1f7321d27539d4faef160f27377d36594
Sergio Sáenz <ssaenz@indaba.es>
2017-04-23 12:16:36 +0200

    Added mvc configuration

--- palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
+++ palabritas-service/src/main/java/org/meleeton/palabritas/config/ServletConfig.java
@@ -2,10 +2,22 @@ package org.meleeton.palabritas.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class ServletConfig extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index.html");
+	}
 }
+
